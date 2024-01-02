d75b255565d949feceeabb9e823383e2ae21c099
Tristen Hogue <smileyhogue@users.noreply.github.com>
2024-01-02 10:22:38 -0500

    compress modal data some more

--- src/app/utils/questionnaireSerializer.ts
+++ src/app/utils/questionnaireSerializer.ts
@@ -7,25 +7,108 @@ import {
   QuestionType,
   SimplifiedQuestion,
   ModalData,
-  SimplifiedModalData,
 } from '@/types/QuestionnaireTypes';
+
+interface SimplifiedModalData {
+  an: string; // Account Name
+  rt: string; // Request Type
+  sa: string; // Same Affiliate
+  ct: string; // Campaign Type
+  at?: string; // ATS Type
+  cs: string[]; // Campaigns
+}
 
 interface CombinedState {
   questions: Question[];
   modalData: ModalData;
 }
 
+const requestTypeMapping: { [key: string]: string } = {
+  'Existing Campaign': 'ec',
+  'New Launch': 'nl',
+};
+
+const campaignTypeMapping: { [key: string]: string } = {
+  'PandoLogic + Easy Apply': 'pea',
+  'A supported ATS': 'ats',
+};
+
+const reverseRequestTypeMapping: { [key: string]: string } = {
+  ec: 'Existing Campaign',
+  nl: 'New Launch',
+};
+
+const reverseCampaignTypeMapping: { [key: string]: string } = {
+  pea: 'PandoLogic + Easy Apply',
+  ats: 'A supported ATS',
+};
+
+const simplifyModalData = (modalData: ModalData): SimplifiedModalData => {
+  return {
+    an: modalData.accountName,
+    rt: requestTypeMapping[modalData.requestType],
+    sa: modalData.sameAffiliate ? 'y' : 'n',
+    ct: campaignTypeMapping[modalData.campaignType],
+    at: modalData.atsType,
+    cs: modalData.campaigns,
+  };
+};
+
 const expandModalData = (
   simplifiedModalData: SimplifiedModalData
 ): ModalData => {
   return {
     accountName: simplifiedModalData.an,
-    requestType: simplifiedModalData.rt,
+    requestType: reverseRequestTypeMapping[simplifiedModalData.rt],
     sameAffiliate: simplifiedModalData.sa,
-    campaignType: simplifiedModalData.ct,
+    campaignType: reverseCampaignTypeMapping[simplifiedModalData.ct],
     atsType: simplifiedModalData.at,
     campaigns: simplifiedModalData.cs,
   };
+};
+
+const simplifyState = (state: Question[]): SimplifiedQuestion[] => {
+  return state.map(
+    ({
+      ExtQuestionID,
+      QuestionText,
+      QuestionType,
+      Required,
+      Min,
+      Max,
+      Limit,
+      Format,
+      Answers,
+    }) => ({
+      i: ExtQuestionID,
+      t: QuestionText,
+      qt: QuestionType,
+      r: Required,
+      mi: Min,
+      ma: Max,
+      l: Limit,
+      f: Format,
+      d: Answers.map((a) => ({ aid: a.ExtAnswerID, at: a.AnswerText })),
+    })
+  );
+};
+
+export const expandState = (
+  simplifiedState: SimplifiedQuestion[]
+): Question[] => {
+  return simplifiedState.map(({ i, t, qt, r, mi, ma, l, f, d }) => ({
+    ExtQuestionID: i || '',
+    QuestionText: t || '',
+    QuestionType: qt as QuestionType,
+    Required: r === true,
+    Min: mi || '',
+    Max: ma || '',
+    Limit: l || 0,
+    Format: f || '',
+    Answers: d
+      ? d.map((a) => ({ ExtAnswerID: a.aid || '', AnswerText: a.at || '' }))
+      : [],
+  }));
 };
 
 export const serializeStateToQueryString = (
@@ -54,68 +137,6 @@ export const parseQueryString = (): CombinedState | null => {
     }
   }
   return null;
-};
-
-const simplifyState = (state: Question[]): SimplifiedQuestion[] => {
-  return state
-    .map(
-      ({
-        ExtQuestionID,
-        QuestionText,
-        QuestionType,
-        Required,
-        Min,
-        Max,
-        Limit,
-        Format,
-        Answers,
-      }) => ({
-        i: ExtQuestionID || undefined,
-        t: QuestionText || undefined,
-        qt: QuestionType || undefined,
-        r: Required || undefined,
-        mi: Min || undefined,
-        ma: Max || undefined,
-        l: Limit || undefined,
-        f: Format || undefined,
-        d: Answers
-          ? Answers.map((a) => ({
-              aid: a.ExtAnswerID || undefined,
-              at: a.AnswerText || undefined,
-            }))
-          : undefined,
-      })
-    )
-    .filter((q) => Object.keys(q).length > 0);
-};
-
-const simplifyModalData = (modalData: ModalData): SimplifiedModalData => {
-  return {
-    an: modalData.accountName,
-    rt: modalData.requestType,
-    sa: modalData.sameAffiliate,
-    ct: modalData.campaignType,
-    at: modalData.atsType || undefined,
-    cs: modalData.campaigns,
-  };
-};
-
-export const expandState = (
-  simplifiedState: SimplifiedQuestion[]
-): Question[] => {
-  return simplifiedState.map(({ i, t, qt, r, mi, ma, l, f, d }) => ({
-    ExtQuestionID: i || '',
-    QuestionText: t || '',
-    QuestionType: qt as QuestionType,
-    Required: r === true,
-    Min: mi || '',
-    Max: ma || '',
-    Limit: l || 0,
-    Format: f || '',
-    Answers: d
-      ? d.map((a) => ({ ExtAnswerID: a.aid || '', AnswerText: a.at || '' }))
-      : [],
-  }));
 };
 
 export const generateShareableUrl = (combinedState: CombinedState): string => {
