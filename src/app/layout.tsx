dc26b7cafc3d48bf993dd78f06620393c5c090db
Tristen Hogue <smileyhogue@users.noreply.github.com>
2024-01-18 21:03:09 -0500

    oops

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>{children}</body>
       <Analytics />
+      <SpeedInsights />
     </html>
   );
 }
