4f31f3bc2591059e529cac3befff7ac4c3c7da30
Tristen Hogue <smileyhogue@users.noreply.github.com>
2024-01-19 16:43:22 -0500

    Test Toast Messages

---
+++ cypress/e2e/testToasts.cy.ts
@@ -0,0 +1,28 @@
+describe('test fail toast', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:3000/');
+    /* ==== Generated with Cypress Studio ==== */
+    cy.get(
+      '.DynamicQuestionnaire_bottomButtonContainer__q40ec > :nth-child(1)'
+    ).click();
+    cy.get('.Toastify__toast-body > :nth-child(2)').should('be.visible');
+    /* ==== End Cypress Studio ==== */
+  });
+
+  /* ==== Test Created with Cypress Studio ==== */
+  it('test download success toast', function () {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.visit('http://localhost:3000');
+    cy.get(':nth-child(2) > .flex').type('test');
+    cy.wait(100);
+    cy.get(
+      '.DynamicQuestionnaire_bottomButtonContainer__q40ec > :nth-child(2)'
+    ).click();
+    cy.get('#accountName').type('test');
+    cy.get('#jobIdentification').select('All Jobs');
+    cy.get('.ShareModal_submitButton__XKHAJ').click();
+    cy.wait(100);
+    cy.get('.Toastify__toast-body > :nth-child(2)').should('be.visible');
+    /* ==== End Cypress Studio ==== */
+  });
+});
